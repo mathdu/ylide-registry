514d4410906b5bb0c5f42d4d56fb2c30ea488922
Mathieu Duchesneau <mathieu@doubledash.io>
2023-07-14 13:41:07 -0400

    WIP draft code to get list of registered addresses in src/app + add dependencies in package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
+import { evmBlockchainFactories, EVMNetwork, REGISTRY_ABI, EVM_CONTRACTS, EthereumBlockchainController, EVM_RPCS } from '@ylide/ethereum'
+import { Ylide } from '@ylide/sdk'
+import Web3 from 'web3'
+import { AbiItem } from 'web3-utils'
+
+import { OwnerList } from './components'
+
 import './App.css'
 
+const maxLogSize = 3000;
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [blockchainController, setBlockchainController] = useState<EthereumBlockchainController>()
+  const [currentLogOffset, setCurrentLogOffset] = useState(0)
+  const [currentLogLatest, setCurrentLogLatest] = useState(0)
+  const [users, setUsers] = useState([]);
+  const [web3, setWeb3] = useState<Web3>();
+
+  Ylide.registerBlockchainFactory(evmBlockchainFactories[EVMNetwork.ETHEREUM])
+
+  const loadBlockchainController = async () => {
+    const controller = await evmBlockchainFactories[EVMNetwork.ETHEREUM].create() as EthereumBlockchainController;
+    setBlockchainController(controller);
+    //setWeb3(controller.web3Readers[0].web3);
+    setWeb3(new Web3(EVM_RPCS[EVMNetwork.ARBITRUM][0].rpc));
+  }
+
+  const getYlideAddresses = async () => {
+    if (!blockchainController || !web3) return
+
+    const latestBlock = await web3.eth.getBlockNumber()
+    console.log(`latest block: ${latestBlock}`)
+
+    setCurrentLogOffset(latestBlock - maxLogSize)
+
+    const contract = new web3.eth.Contract(REGISTRY_ABI.abi as AbiItem[], EVM_CONTRACTS[EVMNetwork.ARBITRUM].registry.address);
+    console.log('contract!', contract)
+
+    // const logs = await web3.eth.getPastLogs({
+    //   topics: [web3.utils.sha3('KeyAttached(address indexed addr, uint256 publicKey, uint64 keyVersion)')]
+    // })
+    const logs = await contract.getPastEvents('KeyAttached', {
+      fromBlock: 'earliest', // currentLogOffset,
+      toBlock: 'latest', // currentLogLatest ?? undefined,
+      // filter: {
+      //  addr: '0x15a33D60283e3D20751D6740162D1212c1ad2a2d'
+      // }
+     });
+    console.log('raw logs!', logs)
+
+    const addresses = await Promise.all(logs.map(async ({ address }) => {
+      console.log(address)
+      const key = await blockchainController.extractPublicKeyFromAddress(address || '')
+      return key
+    }))
+    console.log('Addresses:', addresses)
+    // const filtered = logs.filter((log, index) => addresses[index] != null)
+    // console.log('Filtered:', filtered)
+
+  }
+
+  useEffect(() => {
+    try {
+      loadBlockchainController()
+    }
+    catch(e) {
+      console.error(e)
+    }
+  }, [])
+
+  useEffect(() => {
+    getYlideAddresses()
+  }, [blockchainController])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          pineapples is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <OwnerList />
   )
 }
 
